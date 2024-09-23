7fdaec48bc0e0e826fcf9a33fb4ff4f8ea91bee0
front-bite <SV.Varyachev@gmail.com>
2024-09-23 09:17:38 +0300

    Fix config routes

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), reactScopedCssPlugin()],
-  base: '/at-spec/',
+  base: '/at-spec',
   css: {
     preprocessorOptions: {
       scss: {
